68f83ef20960199ae46c98d4a27f36acb32f6ab0
Andras Serfozo <subztep@gmail.com>
2026-05-27 06:32:55 +0100

    docs(api): scalar renderer

--- apps/api/src/core/routes/reference.ts
+++ apps/api/src/core/routes/reference.ts
@@ -1,24 +1,44 @@
-import { swaggerUI } from "@hono/swagger-ui"
 import { OpenAPIHono } from "@hono/zod-openapi"
+import { Scalar } from "@scalar/hono-api-reference"
 import type { RouteProps } from "#/types"
+import { version } from "../../../package.json"
 
 export const referenceRoutes = new OpenAPIHono<RouteProps>()
 
 // API documentation UI
-if (process.env.NODE_ENV === "development") {
-  referenceRoutes.get("/", swaggerUI({ url: "/reference/openapi.json" }))
-}
+referenceRoutes.get(
+  "/",
+  Scalar({
+    url: "/reference/openapi.json",
+    pageTitle: "Kaja API Reference",
+    theme: "solarized",
+    darkMode: true,
+    hiddenClients: true,
+    hideSearch: true,
+    hideClientButton: true,
+    hideDarkModeToggle: true,
+    hideTestRequestButton: true,
+    documentDownloadType: "none",
+    showDeveloperTools: "never",
+    persistAuth: true,
+    telemetry: false,
+    agent: {
+      disabled: true
+    },
+    mcp: {
+      disabled: true
+    }
+  })
+)
 
-// Setup OpenAPI documentation schema on the main app
+/** Setup OpenAPI documentation schema on the main app. */
 export function setupApiDocs(app: OpenAPIHono<RouteProps>) {
-  if (process.env.NODE_ENV === "development") {
-    app.doc("/reference/openapi.json", {
-      openapi: "3.1.1",
-      info: {
-        title: "Kaja.io API",
-        version: "0.0.1",
-        description: "Custom endpoints reference. [Click here](/auth/reference) for auth endpoints."
-      }
-    })
-  }
+  app.doc("/reference/openapi.json", {
+    openapi: "3.1.1",
+    info: {
+      title: "Kaja.io API",
+      version,
+      description: "Custom endpoints reference. [Click here](/auth/reference) for auth endpoints."
+    }
+  })
 }
