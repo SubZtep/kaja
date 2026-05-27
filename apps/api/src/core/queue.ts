eb4ac24795dc169625e4cd32c56680080a5f09e5
Andras Serfozo <subztep@gmail.com>
2026-05-27 21:40:20 +0100

    chore(api): p-queue log

--- apps/api/src/core/queue.ts
+++ apps/api/src/core/queue.ts
@@ -12,17 +12,15 @@ const queue = new PQueue({ concurrency: 2 })
 
 export const geoipQueue = {
   async add(data: GeoIPJob) {
-    logger.info({ data }, "Adding GeoIP job to queue")
+    logger.trace({ data }, "Adding GeoIP job to queue")
 
     await queue.add(async () => {
       try {
-        logger.trace({ nodeId: data.nodeId, ip: data.ip }, "Processing GeoIP job")
         const location = getGeoLocation(data.ip)
-        logger.trace({ nodeId: data.nodeId, location }, "GeoIP result")
-
-        await nodeService.updateGeoLocation(data.nodeId, location)
-
-        logger.info({ nodeId: data.nodeId }, "GeoIP job completed")
+        if (location) {
+          await nodeService.updateGeoLocation(data.nodeId, location)
+        }
+        logger.info({ nodeId: data.nodeId, location }, "GeoIP job completed")
       } catch (error) {
         logger.error({ error, nodeId: data.nodeId }, "GeoIP job failed")
         throw error
