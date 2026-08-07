56b0e4002779bba5b9f9f7fa2fff81b94ee0c85e
Andras Serfozo <subztep@gmail.com>
2026-08-07 22:47:05 +0100

    feat(web): remove tanstack devtools

--- apps/web/src/components/Providers.tsx
+++ apps/web/src/components/Providers.tsx
@@ -1,9 +1,5 @@
 import { KajaAPI } from "@kaja/sdk"
-import { TanStackDevtools } from "@tanstack/react-devtools"
-import { FormDevtoolsPanel } from "@tanstack/react-form-devtools"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ReactQueryDevtoolsPanel } from "@tanstack/react-query-devtools"
-import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools"
 import { createContext, type ReactNode, useMemo } from "react"
 import { ToastContainer } from "react-toastify"
 import { getAuthClient } from "../hooks/auth-client"
@@ -15,25 +11,6 @@ export function Providers({ apiUrl, children }: Readonly<{ apiUrl: string; child
         {children}
 
         <ToastContainer theme="colored" position="top-center" />
-        <TanStackDevtools
-          config={{
-            position: "bottom-right"
-          }}
-          plugins={[
-            {
-              name: "Tanstack Form",
-              render: <FormDevtoolsPanel />
-            },
-            {
-              name: "Tanstack Router",
-              render: <TanStackRouterDevtoolsPanel />
-            },
-            {
-              name: "Tanstack Query",
-              render: <ReactQueryDevtoolsPanel />
-            }
-          ]}
-        />
       </TanStackQueryProvider>
     </SDKProvider>
   )
