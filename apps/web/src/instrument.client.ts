2de790e90d5c2af46ee09e32e0486f1cc9483aba
Andras Serfozo <subztep@gmail.com>
2026-09-06 20:39:33 +0100

    chore: sentry on prod

--- apps/web/src/instrument.client.ts
+++ apps/web/src/instrument.client.ts
@@ -2,6 +2,7 @@ import * as Sentry from "@sentry/tanstackstart-react"
 
 if (import.meta.env.PROD) {
   Sentry.init({
-    dsn: "https://96f4dd55ad041f9db78a1f9beadc1fac@o326475.ingest.us.sentry.io/4512040899444736"
+    dsn: "https://96f4dd55ad041f9db78a1f9beadc1fac@o326475.ingest.us.sentry.io/4512040899444736",
+    environment: "production"
   })
 }
--- apps/web/src/routeTree.gen.ts
+++ apps/web/src/routeTree.gen.ts
@@ -17,7 +17,6 @@ import { Route as AdminWidgetRouteImport } from './routes/_admin/widget'
 import { Route as PublicIndexRouteImport } from './routes/_public/index'
 import { Route as PublicDeviceRouteImport } from './routes/_public/device'
 import { Route as PublicResetPasswordRouteImport } from './routes/_public/reset-password'
-import { Route as PublicSentryExamplePageRouteImport } from './routes/_public/sentry-example-page'
 import { Route as PublicSigninRouteImport } from './routes/_public/signin'
 import { Route as PublicSignupRouteImport } from './routes/_public/signup'
 import { Route as AdminMcpServersIndexRouteImport } from './routes/_admin/mcp-servers/index'
@@ -63,11 +62,6 @@ const PublicDeviceRoute = PublicDeviceRouteImport.update({
 const PublicResetPasswordRoute = PublicResetPasswordRouteImport.update({
   id: '/reset-password',
   path: '/reset-password',
-  getParentRoute: () => PublicRoute,
-} as any)
-const PublicSentryExamplePageRoute = PublicSentryExamplePageRouteImport.update({
-  id: '/sentry-example-page',
-  path: '/sentry-example-page',
   getParentRoute: () => PublicRoute,
 } as any)
 const PublicSigninRoute = PublicSigninRouteImport.update({
@@ -118,7 +112,6 @@ export interface FileRoutesByFullPath {
   '/widget': typeof AdminWidgetRoute
   '/device': typeof PublicDeviceRouteWithChildren
   '/reset-password': typeof PublicResetPasswordRoute
-  '/sentry-example-page': typeof PublicSentryExamplePageRoute
   '/signin': typeof PublicSigninRoute
   '/signup': typeof PublicSignupRoute
   '/users/$userId': typeof AdminUsersUserIdRoute
@@ -134,7 +127,6 @@ export interface FileRoutesByTo {
   '/profile': typeof AdminProfileRoute
   '/widget': typeof AdminWidgetRoute
   '/reset-password': typeof PublicResetPasswordRoute
-  '/sentry-example-page': typeof PublicSentryExamplePageRoute
   '/signin': typeof PublicSigninRoute
   '/signup': typeof PublicSignupRoute
   '/users/$userId': typeof AdminUsersUserIdRoute
@@ -153,7 +145,6 @@ export interface FileRoutesById {
   '/_admin/widget': typeof AdminWidgetRoute
   '/_public/device': typeof PublicDeviceRouteWithChildren
   '/_public/reset-password': typeof PublicResetPasswordRoute
-  '/_public/sentry-example-page': typeof PublicSentryExamplePageRoute
   '/_public/signin': typeof PublicSigninRoute
   '/_public/signup': typeof PublicSignupRoute
   '/_public/': typeof PublicIndexRoute
@@ -173,7 +164,6 @@ export interface FileRouteTypes {
     | '/widget'
     | '/device'
     | '/reset-password'
-    | '/sentry-example-page'
     | '/signin'
     | '/signup'
     | '/users/$userId'
@@ -189,7 +179,6 @@ export interface FileRouteTypes {
     | '/profile'
     | '/widget'
     | '/reset-password'
-    | '/sentry-example-page'
     | '/signin'
     | '/signup'
     | '/users/$userId'
@@ -207,7 +196,6 @@ export interface FileRouteTypes {
     | '/_admin/widget'
     | '/_public/device'
     | '/_public/reset-password'
-    | '/_public/sentry-example-page'
     | '/_public/signin'
     | '/_public/signup'
     | '/_public/'
@@ -280,13 +268,6 @@ declare module '@tanstack/react-router' {
       path: '/reset-password'
       fullPath: '/reset-password'
       preLoaderRoute: typeof PublicResetPasswordRouteImport
-      parentRoute: typeof PublicRoute
-    }
-    '/_public/sentry-example-page': {
-      id: '/_public/sentry-example-page'
-      path: '/sentry-example-page'
-      fullPath: '/sentry-example-page'
-      preLoaderRoute: typeof PublicSentryExamplePageRouteImport
       parentRoute: typeof PublicRoute
     }
     '/_public/signin': {
@@ -387,7 +368,6 @@ const PublicDeviceRouteWithChildren = PublicDeviceRoute._addFileChildren(
 interface PublicRouteChildren {
   PublicDeviceRoute: typeof PublicDeviceRouteWithChildren
   PublicResetPasswordRoute: typeof PublicResetPasswordRoute
-  PublicSentryExamplePageRoute: typeof PublicSentryExamplePageRoute
   PublicSigninRoute: typeof PublicSigninRoute
   PublicSignupRoute: typeof PublicSignupRoute
   PublicIndexRoute: typeof PublicIndexRoute
@@ -396,7 +376,6 @@ interface PublicRouteChildren {
 const PublicRouteChildren: PublicRouteChildren = {
   PublicDeviceRoute: PublicDeviceRouteWithChildren,
   PublicResetPasswordRoute: PublicResetPasswordRoute,
-  PublicSentryExamplePageRoute: PublicSentryExamplePageRoute,
   PublicSigninRoute: PublicSigninRoute,
   PublicSignupRoute: PublicSignupRoute,
   PublicIndexRoute: PublicIndexRoute,
